f4dde66ef201620639aaa94afdc5deb082f5e8c0
Kuina <75318520+Kuina-chan@users.noreply.github.com>
2024-11-20 12:51:51 +0700

    Delete unnecessary files and/or fixing to fit new tosu quickstart

--- js/odometr.js
+++ js/odometr.js
@@ -10,11 +10,11 @@
   
     FORMAT_MARK_HTML = '<span class="odometer-formatting-mark"></span>';
   
-    DIGIT_FORMAT = '(d ddd)';
+    DIGIT_FORMAT = '( ddd).dd';
   
     FORMAT_PARSER = /^\(?([^)]*)\)?(?:(.)(d+))?$/;
   
-    FRAMERATE = 60;
+    FRAMERATE = 30;
   
     DURATION = 2000;
   
